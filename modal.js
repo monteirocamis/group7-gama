0805a8889812392e8088ff946a366fc2bf37fa88
moca xx <mocaxx@mocas-pro.local>
2024-01-18 12:10:57 -0300

    :art: integrantes details

---
+++ modal.js
@@ -0,0 +1,18 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// class Modal extends React.Component {
+//   state = {
+//     showMessage: false,
+//   };
+//   onButtonClickHandler = () => {
+//     this.setState({ showMessage: !this.state.showMessage });
+//   };
+
+//   render() {
+//     return <div className="Modal">{this.state.showMessage && <p>Hi</p>}</div>;
+//   }
+// }
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<Modal />, rootElement);
